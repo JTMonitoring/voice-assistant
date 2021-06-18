bedce0338230bf092155706386f5e4b610a2841a
JTMonitoring <86123318+JTMonitoring@users.noreply.github.com>
2021-06-18 10:42:43 -0700

    Update chores.py

--- chores.py
+++ chores.py
@@ -1,7 +1,12 @@
-def executechore(transcript):
-    print("Executing .."+transcript)
-    return
 
-def executesudochore(transcript):
-    print("Executing as sudo.."+transcript)
-    return
+
+class chore():
+    def executechore(transcript):
+        print("Executing .."+transcript)
+        if "open Firefox" in transcript:
+            os.system("firefox")
+        return
+
+    def executesudochore(transcript):
+        print("Executing as sudo.."+transcript)
+        return
