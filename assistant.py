35edee4381ddcb2a5f8cc9317ecfef7da06a9cea
dankwaifu <85655669+dankwaifu@users.noreply.github.com>
2021-06-18 09:45:57 -0700

    Add files via upload

---
+++ assistant.py
@@ -0,0 +1,89 @@
+import speech_recognition as sr
+import os.path
+from os import path
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import keyboard
+from playsound import playsound
+from chores import *
+
+r = sr.Recognizer()
+
+def recognize_audio():
+    sound = AudioSegment.from_wav("src/tmp/tempreceive.wav")
+    chunks = split_on_silence(sound, min_silence_len = 500, silence_thresh = sound.dBFS-14, keep_silence = 500)
+    folder_name = "audio-chunks"
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+
+    #process chunks
+    with sr.AudioFile(chunk_filename) as source:
+        audio_listened = r.record(source)
+        try:
+            text = r.recognize_google(audio_listened)
+        except sr.UnknownValueError as e:
+            print("Error: ", str(e))
+        else:
+            text = f"{text.capitalize()}."
+            print(chunk_filename, ":", text)
+            whole_text += whole_text
+    os.system("del src/tmp/tempreceive.wav")
+    return whole_text
+
+
+def save_audio():
+    with sr.Microphone() as source:
+        audio_data = r.record(source, duration=5)
+        print("listening...")
+        text = r.recognize_google(audio_data)
+        temp_file = open("src/tmp/tempreceive.wav")
+        temp_file.write(text)
+        temp_file.close()
+
+while True:
+    try:
+        if keyboard.is_pressed("shift") and keyboard.is_pressed("tab"):
+            save_audio()
+    except:
+        break
+    
+    path = "src/tmp/tempreceive.wav"
+    isdir = os.path.isfile(path)
+    if isdir == True:
+        
+        transcript = recognize_audio()
+        print("[INPUT RECIEVED]:", transcript)
+
+
+        #continuously listen for trigger to access sudo commands
+        trigger = "hey bitch"
+        sudo_trigger = "sudo bitch"
+
+        if trigger in transcript:
+             #alternative to 'hey google' 
+            playsound('src/library/greeting.mp3')
+            try:
+                save_audio()
+            
+            if os.path.isfile("src/tmp/tempreceive.wav"):
+                transcript = recognize_audio()
+                executechore(transcript)
+                
+            except:
+                playsound("src/library/noinput.mp3")
+            
+            
+
+        elif sudo_trigger in transcript:
+            #recognize commands as sudo chores
+            playsound("src/library/greeting.mp3")
+            try:
+                save_audio()
+            
+            if os.path.isfile("src/tmp/tempreceive.wav"):
+                transcript = recognize_audio()
+                executesudochore(transcript)
+                
+            except:
+                playsound("src/library/noinput.mp3")
---
+++ chores.py
@@ -0,0 +1,7 @@
+def executechore(transcript):
+    print("Executing .."+transcript)
+    return
+
+def executesudochore(transcript):
+    print("Executing as sudo.."+transcript)
+    return
